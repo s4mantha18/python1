326584ebc8abce5b945ec56f556e9ba103828e60
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-11 07:47:04 +0100

    first

---
+++ PYTHON.py
@@ -0,0 +1,57 @@
+# print("i love pizza")
+# print("its really good")
+
+# VARIABLES = container for a value, acts as that value
+# name = 'Bro'
+# print("Hello " +name)
+# print(type(name)) # shows the data type
+# INT DATA TYPE
+# first_name = "Bro"
+# # last_name= "Code"
+# # full_name = first_name +" "+ last_name
+# # print("Hello " + full_name)
+# NUMERIC DATA TYPE
+# age = 13
+# age += 1
+# print(age)
+# print(type(age)) # shows data type
+# # cant use string for math
+# # print("Your age is: "+age) # wrong
+# # print("Your age is: "+str(age)) #right
+# FLOAT DATA TYPE
+# height = 250.5
+# print("your height is: "+str(height) +"cm")
+# # print(type(height))
+# BOOLEAN DATA TYPE
+# human = False
+# print("Are you a human: "+str(human))
+# print(type(human))
+
+# MULTIPLE ASSIGNMENT : allows us to assign multiple variables at the same time
+# name = "bro"
+# age = 21
+# attractive = True
+# name, age, attractive = "bro", 21, True
+# print(name)
+# print(age)
+# print(attractive)
+# spongebob = patrick = sandy = squidward = 30
+# print(spongebob)
+# print(patrick)
+# print(sandy)
+# print(squidward)
+
+# string methods in python
+name = "sam"
+# print(len(name)) #find the length of the string
+# print(name.find("a")) #finds the index of the string
+# print(name.capitalize()) #captalises the first letter
+# print(name.upper()) #makes the string uppercase
+# print(name.lower()) #makes everything lowercase
+# print(name.isdigit()) #shows if its a digit
+# print(name.isalpha()) #shows if its an alphabet
+# print(name.count("m")) #shows amount of characters in a string
+# print(name.replace("a", "b")) #replaces letters
+# print(name*3) #lets you print your variable as many times as you want
+
+
